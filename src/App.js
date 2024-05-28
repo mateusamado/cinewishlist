25ca3368f7b7862f8162d2b121d551ab56ecc9e1
mateusamado <mateus.amado@ufms.br>
2024-05-28 05:41:55 -0300

    feat: adicionando testes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./views/Home";
 // import Login from "./views/Login";
 // import Register from "./views/Register";
@@ -8,13 +8,15 @@ import Home from "./views/Home";
 
 function App() {
   return (
-    <Routes>
-      <Route exact path="/" element={<Home />} />
-      {/* <Route path="/login" element={Login} />
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        {/* <Route path="/login" element={Login} />
       <Route path="/register" element={Register} />
       <Route path="/profile" element={Profile} />
       <Route path="/watchlist" element={Watchlist} /> */}
-    </Routes>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
